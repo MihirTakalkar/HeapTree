2c57c6da9e9b9e50aeb0a8fda85bdd45414e054d
Mihir Takalkar <Mihir.Takalkar@GMR.local>
2021-02-14 12:47:44 -0800

    Added HeapifyUp.

---
+++ HeapTree1/HeapTree/Heap.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeapTree
+{
+    class Heap<T> where T: IComparable
+    {
+        List<T> heap = new List<T>();
+        public void Insert(T val)
+        {
+            //add value
+            heap.Add(val);
+            //heapifyUp
+            HeapifyUp(heap.Count - 1);
+        }
+
+        void HeapifyUp(int ind)
+        {
+            if(ind == 0)
+            {
+                //if root is reached
+                return;
+            }
+
+            int parent = (ind - 1) / 2;
+            if (heap[ind].CompareTo(heap[parent]) < 0)
+            {
+                //swap
+                T temp = heap[parent];
+                heap[parent] = heap[ind];
+                heap[ind] = temp;
+                ind = parent;
+            }
+
+            else
+            {
+                //if no more swaps can be made
+                return;
+            }
+
+            //recursive call
+            HeapifyUp(ind);
+        }
+
+
+        T Pop()
+        {
+
+
+        }
+
+
+        void HeapifyDown()
+        {
+
+        }
+
+
+
+    }
+}
